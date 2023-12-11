06ac74e46280e60ca0af944305023ee2eaf9803e
bougheriouHichem <bougheriou1997@gmail.com>
2023-12-11 09:55:21 +0100

    Premier commit

---
+++ src/main/java/com/example/ApiRestPMN/ApiRestPmnApplication.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestPMN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestPmnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestPmnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ApiRestPMN/ApiRestPmnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestPMN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestPmnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
